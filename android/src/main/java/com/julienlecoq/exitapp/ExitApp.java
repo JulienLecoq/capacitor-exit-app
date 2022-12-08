ba4c7eda2ba8212f435fbfbfbc99cf6238f9d741
Julien Lecoq <julien_lecoq@protonmail.com>
2022-12-08 02:22:47 +0100

    Initial commit

---
+++ android/src/main/java/com/julienlecoq/exitapp/ExitApp.java
@@ -0,0 +1,11 @@
+package com.julienlecoq.exitapp;
+
+import android.util.Log;
+
+public class ExitApp {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
