4fa6a998fe4902172b512e47ad033ecf15408827
Julien Lecoq <julien_lecoq@protonmail.com>
2022-12-08 02:41:05 +0100

    small refactor

--- ios/Plugin/ExitAppPlugin.swift
+++ ios/Plugin/ExitAppPlugin.swift
@@ -4,16 +4,14 @@ import Capacitor
 @objc(ExitAppPlugin)
 public class ExitAppPlugin: CAPPlugin {
     @objc func killApp(_ call: CAPPluginCall) {
-//        DispatchQueue.main.async {
-//        }
         return MinimizeOrKillApp();
     }
     
     @IBAction func MinimizeOrKillApp(){
         DispatchQueue.main.async {
-           // UI work here
+            // UI work here
             UIControl().sendAction(#selector(URLSessionTask.suspend), to: UIApplication.shared, for: nil)
-            //Comment if you want to minimise app
+            // Comment if you want to minimise app
             Timer.scheduledTimer(withTimeInterval: 0.2, repeats: false) { (timer) in
                 exit(0)
             }
